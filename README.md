421c0daa4946733c76fcfb16e84a3e4c6518007b
Darren Pham <phamdarren1@gmail.com>
2019-04-11 22:41:19 -0700

    finished the game: game functionality, css, html, and added comments

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # unit-4-game
-Game created during the 4th week of bootcamp
+A short mini game created during the fourth week of my Bootcamp
+
+Goal: Defeat all your enemies. All their HP hits 0 while your fighter stays alive.
+
+Intructions:
+1. Select your fighter (click on any of the available 4 different characters)
+2. Select the enemy you want to fight (click on any of the available enemies on the bottom row)
+3. Fight! (click the fight button)
+    NOTE: Damage is calculated randomly, but you should have the ability to win; some luck is required :)
+4. Continue to fight! (After you defeat an enemy, continue to click on the next one or pick a different one until there are none left)
+5. ???
+6. Profit! (JK, either you win or lose the game, click Try Again to play again)
--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -34,75 +34,162 @@ intiateGame = {
     // Nested forEach functions to create divs with appropiate styling for each character
     resetCharacters : function() {
         Object.entries(characters).forEach(([key, value]) => {
-            var fighterCard = $("<div>");
-            fighterCard.addClass("fighter availableFighters text-center");
+            var fighterCard = $('<div>');
+            fighterCard.addClass('fighter availableFighters text-center');
             Object.entries(value).forEach(([key, value]) => {
                 //Add attributes of each fighter their respective elements
                 fighterCard.attr(key, value);
                 //Function to add html elements to fighter div
                 switch (key) {
-                    case "name":
+                    case 'name':
                         fighterCard.text(value);
                         break;
-                    case "picture":
+                    case 'picture':
                         fighterCard.append('<img class="fighterImage" src="' +value+ '" />');
                         break;
-                    case "hp":
-                        fighterCard.append('<div class="figherHP">HP: ' +value+ '</div>')
+                    case 'hp':
+                        fighterCard.append('<div class="fighterHP">HP: ' +value+ '</div>');
                 };
-                $("#topRow").append(fighterCard);
+                // Adds the character created to the top row
+                $('#topRow').append(fighterCard);
             });
         }); 
     },
-
+    // Resets the game if player wins/loses
+    resetGame : function() {
+        location.reload();
+    },
 };
 
-charactersMutation = {
-    attack : function(){
-
-    },
-    lostHealth :  function() {
-
+characterMutation = {
+    // Updates character div and text with new health
+    loseHealth :  function(fighter, dmg) {
+        var newHP = $(fighter).attr('hp') - dmg;
+        $(fighter).attr('hp',newHP);
+        $(fighter).find('.fighterHP').text("HP: " + newHP);
+        gameEvents.checkGameState();
     },
 };
 
 gameEvents = {
+    // Updates backend info for character player chooses and makes the rest enemies
     fighterSelect : function() {
+        $('#selectFighter').removeClass('hidden');
         $(document).on('click', '.availableFighters', function(){
             // console.log(this)
+            $('#selectFighter').addClass('hidden');
             var thisFighter = $(this).attr('Name');
             characters[thisFighter].selected = true;
             console.log("Player Selected " + $(this).attr('Name'));
             gameEvents.moveOpponentsBottom($(this).attr('Name'));
-            $("#selectFighter").hide();
-            $(".availableFighters").removeClass("availableFighters").addClass("enemyFighters");
-            $(this).removeClass("enemyFighters").addClass("yourFighter");
+            $('.availableFighters').removeClass('availableFighters').addClass('enemyFighters');
+            $(this).removeClass('enemyFighters').addClass('yourFighter');
+            $('#selectEnemy').removeClass("hidden");
         });
     },
+    // Move the player's opponents to the correct place (bottom row)
     moveOpponentsBottom :function(playerFighter) {
         Object.keys(characters).forEach(function (fighter) {
             if (fighter != playerFighter) {
-                $("#botRow").append($("div[Name='" + fighter +"']"))
+                $('#botRow').append($("div[Name='" + fighter +"']"));
+            };
+        });
+    },
+    // Listener function for when player chooses the enemy
+    opponentSelect : function() {
+        $('#selectFighter').removeClass('hidden');
+        $(document).on('click', '.enemyFighters', function(){
+            if ( $('#opponentZone').contents().length == 0 ) {
+                var thisOpponent = $(this).attr('Name');
+                // console.log(this)
+                characters[thisOpponent]['enemy'] = true;
+                $('#opponentZone').append($("div[Name='" + thisOpponent +"']"));
+                $('#selectEnemy').addClass("hidden");
+                $('#fightButton').removeClass("hidden");
+            } else {
+                gameEvents.switchOpponent(this);
             }
         });
     },
-    opponentSelect : function() {
-        $("#selectEnemy").show();
-        $(document).on('click', '.enemyFighers', function(){
-            var thisOpponent = $(this).attr('Name');
-            // console.log(this)
-            // characters[thisOpponent]["enemy"] = true;
-            console.log(characters);
+    // listener function if player decides to choose a different opponent
+    switchOpponent : function(switchEnemy) {
+        $('#botRow').append($('#opponentZone').children());
+        var switchEnemyName= $(switchEnemy).attr('Name');
+        $('#opponentZone').append($("div[Name='" + switchEnemyName +"']"));
+        console.log(switchEnemyName)
+    },
+    // Fight button
+    fight : function() {
+        $('#fightButton').on('click', function() {
+            var yourFighter =  $('#topRow').find('.fighter');
+            var enemyFighter = $('#opponentZone').find('.fighter');
+
+            characterMutation.loseHealth(enemyFighter,gameEvents.calculateDamage(yourFighter));
+            characterMutation.loseHealth(yourFighter,gameEvents.calculateDamage(enemyFighter));
         });
     },
-    battle : function() {
-
+    // function to calculate damage after player fights
+    calculateDamage : function(fighter) {
+        var dps = fighter.attr('dps');
+        if ( $(fighter).hasClass('yourFighter') ) {
+            var mulitplier = 40 + (.1 * Math.floor(Math.random() * Math.floor(10)));
+            damage = Math.round(dps * mulitplier);
+            console.log(damage);
+            return damage
+        } else {
+            var mulitplier = 1 + (.1 * Math.floor(Math.random() * Math.floor(10)));
+            damage = Math.round(dps * mulitplier);
+            console.log(damage);
+            return damage
+        }
+        
     },
+    // checks the current game state (around the hp of the player's fighter and current enemy)
+    checkGameState : function() {
+        var yourFighter =  $('#topRow').find('.fighter');
+        var enemyFighter = $('#opponentZone').find('.fighter');
+
+        var yourFighterData = {
+            name : yourFighter.attr('Name'),
+            hp : yourFighter.attr('hp'),
+        }
+        var yourEnemyData = {
+            name : enemyFighter.attr('Name'),
+            hp : enemyFighter.attr('hp'),
+        }
+
+        // win and lose conditions are here; checks HPs and enemies left
+        if ( enemyFighter.attr('hp') <= 0 && $('#botRow').contents().length == 0 ) {
+            $('#opponentZone').empty();
+            gameEvents.win();
+        } else if ( enemyFighter.attr('hp') <= 0 ){
+            $('#opponentZone').empty();
+            $('#selectEnemy').removeClass("hidden");
+            $('#fightButton').addClass("hidden");
+            gameEvents.opponentSelect;
+        } else if (yourFighter.attr('hp') <= 0){
+            $('#topRow').empty();
+            gameEvents.lose();
+        }
+    },
+    // Win function
     win : function() {
         console.log("you win");
+        $('#fightButton').addClass("hidden");
+        $('#fightButton').addClass("hidden");
+        $('#winplayAgain').removeClass("hidden");
+        $('#winplayAgain').on('click', function() {
+            intiateGame.resetGame();
+        });
     },
+    // Lose function
     lose : function() {
         console.log("you lose");
+        $('#fightButton').addClass("hidden");
+        $('#loseplayAgain').removeClass("hidden");
+        $('#loseplayAgain').on('click', function() {
+            intiateGame.resetGame();
+        });
     }
 };
 
@@ -110,3 +197,4 @@ gameEvents = {
 intiateGame.resetCharacters();
 gameEvents.fighterSelect();
 gameEvents.opponentSelect();
+gameEvents.fight();
